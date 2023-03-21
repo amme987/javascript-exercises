191ab81911453e12e4883efd4aecaf36d5a976cc
amme987 <emmagination97@gmail.com>
2023-03-21 18:07:53 -0400

    Complete exercises 8-9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  word = word.match(/\w/g).join("").toLowerCase();
+  let palindrome = [];
+  for (let i = word.length - 1; i >= 0; i--) {
+    palindrome.push(word[i]);
+  }
+  if (palindrome.join("") === word) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
