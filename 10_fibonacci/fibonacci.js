3c6043437f8674307263f6b1de13c9f2f873aa43
amme987 <emmagination97@gmail.com>
2023-03-22 08:13:40 -0400

    Complete exercises 10-12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let first = 1,
+    second = 1;
+  let sum;
+  if (num < 0) {
+    return "OOPS";
+  }
+  if (num == 0) {
+    return 0;
+  }
+  if (num == 1 || num == 2) {
+    return 1;
+  }
+  for (let i = 3; i <= num; i++) {
+    sum = first + second;
+    first = second;
+    second = sum;
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  for (item of arr) {
+    console.log(item.name);
+    if (!item.hasOwnProperty("yearOfDeath")) {
+      item.yearOfDeath = new Date().getFullYear();
+    }
+  }
+  const sort = arr.sort((a, b) => {
+    return a.yearOfDeath - a.yearOfBirth - (b.yearOfDeath - b.yearOfBirth);
+  });
+  return sort[sort.length - 1];
 };
 
 // Do not edit below this line
